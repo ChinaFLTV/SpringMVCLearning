437ad7c16a00aa09be4f3af20130ea55004b7e8c
ChinaFLTV <236010069@qq.com>
2022-09-21 18:07:38 +0800

    2022-9-21  18:07-Day2-2

--- Day2/src/main/java/controller/HahaController.java
+++ Day2/src/main/java/controller/HahaController.java
@@ -11,8 +11,12 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 
 /**
  * @author Lenovo
@@ -56,6 +60,73 @@ public class HahaController {
     @RequestMapping(value = "/xixixi", method = {RequestMethod.GET}, params = {"username", "password=123456"}, headers = {"host=localhost:8080"})
     public String xixixi() {
 
+        return "success";
+
+    }
+
+    @RequestMapping(value = "/a?a/aha", method = {RequestMethod.GET}, params = "username", headers = {"host=localhost:8080"})
+    public String eee() {
+
+        return "success";
+
+    }
+
+
+    @RequestMapping(value = "/he*tui", method = {RequestMethod.GET}, params = {"username=Dage"}, headers = {"host=localhost:8080"})
+    public String hetui() {
+
+        return "success";
+
+    }
+
+
+    @RequestMapping(value = "/**/ati", method = {RequestMethod.GET}, params = {"username=Dage"}, headers = {"host=localhost:8080"})
+    public String ati() {
+
+        return "success";
+
+    }
+
+    @RequestMapping(value = "/hehehe*/hehe/{username}/{password}", method = {RequestMethod.GET}, params = {"!username"}, headers = {"host=localhost:8080"})
+    public String placeholderTest(@PathVariable("username") String username, @PathVariable("password") String password) {
+
+        System.out.println("username = " + username + " password = " + password);
+        return "success";
+
+    }
+
+
+    @RequestMapping(value = "/abc")
+    public String abc() {
+
+        return "testParams";
+
+    }
+
+
+    @RequestMapping(value = "/aihei")
+    public String aihei(HttpServletRequest request) {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        System.out.println("username = " + username + " password = " + password);
+        return "success";
+
+    }
+
+    @RequestMapping(value = "/abcdef")
+    public String abcdef(String username, String password) {
+
+        System.out.println("username = " + username + " password = " + password);
+        return "success";
+
+    }
+
+
+    @RequestMapping(value = "/abcdefgh", method = {RequestMethod.POST})
+    public String abcdefgh(String username, String password, String hobby) {
+
+        System.out.println("username = " + username + " password = " + password + " hobby = " + hobby);
         return "success";
 
     }
